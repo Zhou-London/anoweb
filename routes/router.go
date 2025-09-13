1ed8b9ee8cc387806ec373546bd2b08ef42a2920
zhou <zhouzhou.zhang.24@ucl.ac.uk>
2025-09-13 17:59:01 +0000

    Updates the router

--- routes/router.go
+++ routes/router.go
@@ -2,11 +2,29 @@ package routes
 
 import (
 	"anonchihaya.co.uk/handlers"
+	"anonchihaya.co.uk/repositories"
 	"github.com/gin-gonic/gin"
 )
 
 var prefix string = "/api"
 
-func InitRoutes(r *gin.Engine) {
+func InitRoutes(r *gin.Engine,
+	profile_repo repositories.ProfileRepository,
+) {
+
+	// * Home
 	r.GET(prefix+"/home", handlers.GetHomeMsg)
+	r.POST(prefix+"/home"+"/upload-profile-img", handlers.PostProfileImg)
+	r.GET(prefix+"/home"+"/profile-info", func(ctx *gin.Context) {
+		handlers.GetProfileInfo(ctx, profile_repo)
+	})
+	r.DELETE(prefix+"/home"+"/profile-info", func(ctx *gin.Context) {
+		handlers.DeleteProfileInfo(ctx, profile_repo)
+	})
+	r.POST(prefix+"/home"+"/profile-info", func(ctx *gin.Context) {
+		handlers.PostProfileInfo(ctx, profile_repo)
+	})
+	r.PUT(prefix+"/home"+"/profile-info", func(ctx *gin.Context) {
+		handlers.PutProfileInfo(ctx, profile_repo)
+	})
 }
