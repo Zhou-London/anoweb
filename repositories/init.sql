ed90d5711ce4afbb4cfeba83238503d48af240b9
zhou <zhouzhou.zhang.24@ucl.ac.uk>
2025-09-14 18:41:56 +0000

    Updates the sql script

--- repositories/init.sql
+++ repositories/init.sql
@@ -1,11 +1,29 @@
 CREATE DATABASE IF NOT EXISTS anoweb;
 USE anoweb;
-DROP TABLE IF EXISTS profile_info;
-CREATE TABLE profile_info (
+CREATE TABLE IF NOT EXISTS profile_info (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50),
     email VARCHAR(100),
     github VARCHAR(100),
     linkedin VARCHAR(100),
     bio VARCHAR(200)
+);
+CREATE TABLE IF NOT EXISTS experiences (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    company VARCHAR(100),
+    position VARCHAR(100),
+    start_date DATE,
+    end_date DATE,
+    present BOOLEAN,
+    description TEXT,
+    image_url VARCHAR(200)
+);
+CREATE TABLE IF NOT EXISTS educations (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    school VARCHAR(100),
+    degree VARCHAR(100),
+    start_date DATE,
+    end_date DATE,
+    link VARCHAR(200),
+    image_url VARCHAR(200)
 );
