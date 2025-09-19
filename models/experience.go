ac4021b78a5c0b62ae2f1b39216b491838ae1100
zhou <zhouzhou.zhang.24@ucl.ac.uk>
2025-09-19 21:14:29 +0000

    Modifies the struct of experience with new field order_index

--- models/experience.go
+++ models/experience.go
@@ -9,14 +9,16 @@ type Experience struct {
 	Present     bool   `json:"present"`
 	Description string `json:"description"`
 	ImageURL    string `json:"image_url"`
+	OrderIndex  string `json:"order_index"`
 }
 
 type ExperienceShort struct {
-	ID        int    `json:"id"`
-	Company   string `json:"company"`
-	Position  string `json:"position"`
-	StartDate string `json:"start_date"`
-	EndDate   string `json:"end_date"`
-	Present   bool   `json:"present"`
-	ImageURL  string `json:"image_url"`
+	ID         int    `json:"id"`
+	Company    string `json:"company"`
+	Position   string `json:"position"`
+	StartDate  string `json:"start_date"`
+	EndDate    string `json:"end_date"`
+	Present    bool   `json:"present"`
+	ImageURL   string `json:"image_url"`
+	OrderIndex string `json:"order_index"`
 }
