4c2143407536485daf9f83efbb3006470e5bc1c1
zhou <zhouzhou.zhang.24@ucl.ac.uk>
2025-09-21 23:56:37 +0000

    Updates the route

--- static/anoweb-front/src/App.tsx
+++ static/anoweb-front/src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router";
-import Home from "./Pages/home";
-import About from "./Pages/about";
+import About from "./Pages/About/about";
+import Home from "./Pages/Home/home";
+import ProjectPage from "./Pages/Projects";
 
 export default function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/projects" element={<ProjectPage />} />
     </Routes>
   );
 }
