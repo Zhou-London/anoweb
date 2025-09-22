93361626328b42f31c8cd734be2d8d4ccd6fc865
zhou <zhouzhou.zhang.24@ucl.ac.uk>
2025-09-22 14:06:04 +0000

    Comments out the auto-generated field

--- models/projects.go
+++ models/projects.go
@@ -1,11 +1,12 @@
 package models
 
 type Project struct {
-	ID          int    `json:"id"`
+	ID          int    `json:"id"` // Auto
 	Name        string `json:"name"`
 	Description string `json:"description"`
 	Link        string `json:"link"`
 	ImageURL    string `json:"image_url"`
-	CreatedAt   string `json:"created_at"`
-	UpdatedAt   string `json:"updated_at"`
+
+	CreatedAt string `json:"created_at"` // Auto
+	UpdatedAt string `json:"updated_at"` // Auto
 }
